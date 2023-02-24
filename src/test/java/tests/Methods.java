5afdaa1f9ffd28abebe29f5b28c26fae3cb66298
preethiapr <preethi.apr22@gmail.com>
2023-02-24 09:48:29 +0530

    My project from Eclipse

---
+++ src/test/java/tests/Methods.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Methods {
+	
+	public void test() {
+		System.out.println("Inside");
+	}
+
+	public static void main(String[] args) {
+		Methods ma = new Methods();
+		ma.test();
+
+	}
+
+}
