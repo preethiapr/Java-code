7078dec79d9b211ff878740efd9e36dc57ab9778
preethiapr <preethi.apr22@gmail.com>
2023-02-26 18:59:48 +0530

    New changes in my code

---
+++ src/test/java/tests/StaticDropdown.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class StaticDropdown {
+
+	public static void main(String[] args) {
+		
+		String str = "Selenium";
+		char[] ch = str.toCharArray();
+		for(int i=0; i<=ch.length-1; i++) {
+		System.out.println(ch[i]);
+
+	}
+}
+}
